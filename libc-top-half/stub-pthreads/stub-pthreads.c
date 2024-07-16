9eb046b1757ff142f835b6f7b12d3efdb1de9df8
R <rqou@berkeley.edu>
2024-07-16 06:30:51 +0100

    turns out mutex timed wait is easy

--- libc-top-half/stub-pthreads/stub-pthreads.c
+++ libc-top-half/stub-pthreads/stub-pthreads.c
@@ -77,3 +77,9 @@ int pthread_mutex_unlock(pthread_mutex_t *m)
 	m->_m_count--;
 	return 0;
 }
+int pthread_mutex_timedlock(pthread_mutex_t *restrict m, const struct timespec *restrict at)
+{
+	/* "The pthread_mutex_timedlock() function may fail if: A deadlock condition was detected." */
+	/* This means that we don't have to wait and then return timeout, we can just detect deadlock. */
+	return pthread_mutex_lock(m);
+}
