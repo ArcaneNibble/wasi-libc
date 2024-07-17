69893d0b2a31ffb948ab0e47a8dd84d964097906
R <rqou@berkeley.edu>
2024-07-17 19:56:15 +0100

    rwlock has __ prefix

--- libc-top-half/stub-pthreads/stub-pthreads.c
+++ libc-top-half/stub-pthreads/stub-pthreads.c
@@ -125,41 +125,49 @@ int pthread_cond_timedwait(pthread_cond_t *restrict c, pthread_mutex_t *restrict
 
 /* Musl uses bit31 to mark "has waiters", bit[30:0] all 1s to indicate writer */
 
-int pthread_rwlock_rdlock(pthread_rwlock_t *rw)
+/* These functions have the __ prefix just to stub out thread-specific data */
+
+int __pthread_rwlock_rdlock(pthread_rwlock_t *rw)
 {
 	if (rw->_rw_lock == 0x7fffffff) return EDEADLK;
 	if (rw->_rw_lock == 0x7ffffffe) return EAGAIN;
 	rw->_rw_lock++;
 	return 0;
 }
-int pthread_rwlock_timedrdlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
+weak_alias(__pthread_rwlock_rdlock, pthread_rwlock_rdlock);
+int __pthread_rwlock_timedrdlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
 {
 	return pthread_rwlock_rdlock(rw);
 }
-int pthread_rwlock_tryrdlock(pthread_rwlock_t *rw)
+weak_alias(__pthread_rwlock_timedrdlock, pthread_rwlock_timedrdlock);
+int __pthread_rwlock_tryrdlock(pthread_rwlock_t *rw)
 {
 	if (rw->_rw_lock == 0x7fffffff) return EBUSY;
 	if (rw->_rw_lock == 0x7ffffffe) return EAGAIN;
 	rw->_rw_lock++;
 	return 0;
 }
-int pthread_rwlock_wrlock(pthread_rwlock_t *rw)
+weak_alias(__pthread_rwlock_tryrdlock, pthread_rwlock_tryrdlock);
+int __pthread_rwlock_wrlock(pthread_rwlock_t *rw)
 {
 	if (rw->_rw_lock) return EDEADLK;
 	rw->_rw_lock = 0x7fffffff;
 	return 0;
 }
-int pthread_rwlock_timedwrlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
+weak_alias(__pthread_rwlock_wrlock, pthread_rwlock_wrlock);
+int __pthread_rwlock_timedwrlock(pthread_rwlock_t *restrict rw, const struct timespec *restrict at)
 {
 	return pthread_rwlock_wrlock(rw);
 }
-int pthread_rwlock_trywrlock(pthread_rwlock_t *rw)
+weak_alias(__pthread_rwlock_timedwrlock, pthread_rwlock_timedwrlock);
+int __pthread_rwlock_trywrlock(pthread_rwlock_t *rw)
 {
 	if (rw->_rw_lock) return EBUSY;
 	rw->_rw_lock = 0x7fffffff;
 	return 0;
 }
-int pthread_rwlock_unlock(pthread_rwlock_t *rw)
+weak_alias(__pthread_rwlock_trywrlock, pthread_rwlock_trywrlock);
+int __pthread_rwlock_unlock(pthread_rwlock_t *rw)
 {
 	if (rw->_rw_lock == 0x7fffffff)
 		rw->_rw_lock = 0;
@@ -167,6 +175,7 @@ int pthread_rwlock_unlock(pthread_rwlock_t *rw)
 		rw->_rw_lock--;
 	return 0;
 }
+weak_alias(__pthread_rwlock_unlock, pthread_rwlock_unlock);
 
 /* Spinlocks */
 
